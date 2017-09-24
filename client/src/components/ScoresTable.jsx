60bb6f328661e6cfaa639765647a5160813a6a10
amatranga <amatranga892@gmail.com>
2017-09-24 10:44:54 -0700

    Update to ScoresTable component

--- client/src/components/ScoresTable.jsx
+++ client/src/components/ScoresTable.jsx
@@ -1,40 +1,67 @@
 import React from 'react';
-import HoleNumber from './HoleNumber';
-import Player from './Player';
+import ScoreTableTop from './ScoreTableTop';
+import ScoreTableBottom from './ScoreTableBottom';
 
-const ScoresTable = (props) => {
-  return (
-    <div>
-      <table className="table table-striped table-responsive">
-        <thead>
-          <tr>
-            {props.holes.map((holeNumber, idx) =>
-              idx < 11 ? <HoleNumber key={idx} num={idx} holeNumber={props.holes[idx]} /> : null
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {props.playerNames.map((player, idx) =>
-            <Player key={idx} id={idx} name={player} form={true} />
-          )}
-        </tbody>
-      </table>
-      <table className="table table-striped table-responsive">
-        <thead>
-          <tr>
-            {props.holes.map((holeNumber, idx) =>
-              idx > 10 ? <HoleNumber key={idx} num={idx} holeNumber={props.holes[idx]} /> : null
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {props.playerNames.map((player, idx) =>
-            <Player key={idx} id={idx} name={player} form={true} />
-          )}
-        </tbody>
-      </table>
-    </div>
-  );
+class ScoresTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scoreTableTop: [],
+      scoreTableBottom: []
+    }
+
+    this.setTopScore = this.setTopScore.bind(this);
+    this.setBottomScore = this.setBottomScore.bind(this);
+  }
+
+  setTopScore(totalScore, playerId) {
+    let scoreTableTop = this.state.scoreTableTop;
+    let score = {totalScore, playerId};
+    scoreTableTop.push(score);
+    this.setState({scoreTableTop});
+  }
+
+  setBottomScore(totalScore, playerId) {
+    let scoreTableBottom = this.state.scoreTableBottom;
+    let score = {totalScore, playerId};
+    scoreTableBottom.push(score);
+    this.setState({scoreTableBottom});
+  }
+
+  render() {
+    return (
+      <div>
+        <ScoreTableTop holes={this.props.holes.slice(0, 11)} playerNames={this.props.playerNames} setTopScore={this.setTopScore} />
+        <ScoreTableBottom holes={this.props.holes.slice(11, 22)} playerNames={this.props.playerNames} setBottomScore={this.setBottomScore} />
+      </div>
+    );
+  }
+  
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // console.log(nextState, 'nextState =============== \n', this.state, 'current state');
+  //   let curScoresTop = JSON.stringify(this.state.scoreTableTop);
+  //   let curScoresBottom = JSON.stringify(this.state.scoreTableBottom)
+  //   let nextScoresTop = JSON.stringify(nextState.scoreTableTop);
+  //   let nextScoresBottom = JSON.stringify(nextState.scoreTableBottom);
+  //   return (curScoresTop !== nextScoresTop || curScoresBottom !== nextScoresBottom);
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(nextProps, 'nextProps');
+  //   console.log(this.props, 'curProps');
+  //   console.log(nextState, 'nextState');
+  //   console.log(this.state, 'curState');
+  //   return false;
+  // }
+
+  componentDidUpdate() {
+    const numberOfPlayers = this.props.playerNames.length;
+    let scores = [];
+    let top = [...this.state.scoreTableTop].slice(-numberOfPlayers);
+    let bottom = [...this.state.scoreTableBottom].slice(-numberOfPlayers);
+    scores.push({top, bottom});
+    this.props.updatePlayerRankings(scores);
+  }
 }
 
 export default ScoresTable;
