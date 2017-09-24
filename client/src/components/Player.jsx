3741c8b74168efb6051327197f990f281ab24e94
amatranga <amatranga892@gmail.com>
2017-09-24 10:43:32 -0700

    Update to Player component

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -8,17 +8,23 @@ class Player extends React.Component {
       id: this.props.id,
       totalScore: 0,
       name: this.props.name.playerName,
-      scores: [0,0,0,0,0,0,0,0,0]
+      scores: [0,0,0,0,0,0,0,0,0],
     }
 
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(score, idx) {
+  handleChange(score, idx, playerId) {
     let scores = this.state.scores;
     const totalScore = this.state.totalScore + (score - scores[idx]);
     scores[idx] = score;
     this.setState({totalScore, scores});
+
+    if (this.props.position === "top") {
+      this.props.setTopScore(totalScore, playerId);
+    } else if (this.props.position === "bottom") {
+      this.props.setBottomScore(totalScore, playerId);
+    }
   }
 
   render() {
@@ -33,15 +39,24 @@ class Player extends React.Component {
         </tr>
       );
     } else {
+      let total = 0;
+      if (this.props.totalScoresTop.totalScore) {
+        let totalScoresTop = this.props.totalScoresTop[this.state.id];
+      }
+      if (this.props.totalScoresBottom.totalScore) {
+        let totalScoresBottom = this.props.totalScoresBottom[this.state.id];
+      }
+
+      console.log(totalScoresTop, totalScoresBottom);
       return (
         <tr>
           <th scope="row" id={this.state.id}>{this.state.id + 1}</th>
           <td>{this.state.name}</td>
-          <td>Total</td>
+          <td>{totalScoresTop.totalScore + totalScoresBottom.totalScore || 0}</td>
         </tr>
       );
     }
-  } 
+  }
 }
 
 export default Player;
