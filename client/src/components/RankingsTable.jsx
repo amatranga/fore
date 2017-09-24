120f1dee8895134faeafeddfb564b781056fec10
amatranga <amatranga892@gmail.com>
2017-09-24 10:43:53 -0700

    Update to RankingsTable component

--- client/src/components/RankingsTable.jsx
+++ client/src/components/RankingsTable.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import Player from './Player';
 
-const RankingsTable = (props) => {
-  return (
-    <div>
-      <table className="table table-striped table-bordered table-sm">
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th>Player</th>
-            <th>Score</th>                        
-          </tr>
-        </thead>
-        <tbody>
-          {props.playerNames.map((player, idx) =>
-            <Player key={idx} id={idx} name={player} form={false} />
-          )}
-        </tbody>
-      </table>
-    </div>
-  );
+class RankingsTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props, 'props in RankingsTable');
+    return (
+      <div>
+        <table className="table table-striped table-bordered table-sm">
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Player</th>
+              <th>Score</th>                        
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.playerNames.map((player, idx) =>
+              <Player key={idx} id={idx} name={player} form={false} totalScoresTop={this.props.totalScoresTop} totalScoresBottom={this.props.totalScoresBottom} />
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  componentDidUpdate() {
+    let totalScores = this.props.totalScores;
+    console.log(totalScores, 'totalScores in RankingsTable');
+  }
 }
 
 export default RankingsTable;
